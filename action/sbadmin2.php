4060aaeb1ec540d2603c3269c057b0e93485812e
Michele Azzolari <michele@azzolari.it>
2015-09-29 10:52:20 +0200

    Added an example using sbadmin2

---
+++ action/sbadmin2.php
@@ -0,0 +1,50 @@
+<?php
+if (! defined ( 'FLUIDFRAME' )) {
+    exit ( 1 );
+}
+class Sbadmin2Action extends Action {
+    protected $renderOptions = array ();
+    function prepare($args) {
+        parent::prepare ( $args );
+        $this->prepareMenu();
+    }
+    function handle() {
+    }
+    function prepareMenu() {
+        $menu = array (
+                array (
+                        'class' => 'fa-dashboard',
+                        'href' => common_get_route ( 'home', array (
+                                'lang' => $this->lang 
+                        ) ),
+                        'title' => 'Dashboard' 
+                ),
+                array (
+                        'class' => 'fa-bar-chart-o',
+                        'href' => '#cazz',
+                        'title' => 'File',
+                        'items' => array (
+                                array (
+                                        'href' => common_get_route ( 'home', array (
+                                                'lang' => $this->lang 
+                                        ) ),
+                                        'title' => 'Images' 
+                                ),
+                                array (
+                                        'href' => common_get_route ( 'home', array (
+                                                'lang' => $this->lang 
+                                        ) ),
+                                        'title' => 'Attachments' 
+                                ) 
+                        ) 
+                ),
+                array (
+                        'class' => 'fa-table',
+                        'href' => '#',
+                        'title'=> 'Tables' 
+                ) 
+        );
+        
+        $this->renderOptions ['menu'] = $menu;
+    }
+}
