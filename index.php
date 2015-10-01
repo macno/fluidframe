a3031085c5d3376a7ccf626b07b64aad8a1edcc5
Michele Azzolari <michele@azzolari.it>
2015-10-01 17:38:25 +0200

    Removed useless log

--- index.php
+++ index.php
@@ -1,76 +1,72 @@
 <?php
-define('FLUIDFRAME',true);
-define('INSTALLDIR', dirname(__FILE__));
+define ( 'FLUIDFRAME', true );
+define ( 'INSTALLDIR', dirname ( __FILE__ ) );
 
-require_once(INSTALLDIR.'/lib/core.php');
-
-
+require_once (INSTALLDIR . '/lib/core.php');
 
 try {
-    Fluidframe::init();
-    $_lang = common_language();
-} catch(Exception $e) {
-    common_error($e->getTraceAsString());
-    $error = new ErrorAction('en');
-    $error->setErrorMessage(500, $e->getMessage());
-    $error->handle();
+    Fluidframe::init ();
+    $_lang = common_language ();
+} catch ( Exception $e ) {
+    common_error ( $e->getTraceAsString () );
+    $error = new ErrorAction ( 'en' );
+    $error->setErrorMessage ( 500, $e->getMessage () );
+    $error->handle ();
 }
 
 // get the incoming request URL path
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$path = parse_url ( $_SERVER ['REQUEST_URI'], PHP_URL_PATH );
 
 // get the route based on the path and server
-$route = $router->match($path, $_SERVER);
+$route = $router->match ( $path, $_SERVER );
 
-if(empty($route)) {
-    $error = new ErrorAction($_lang);
-    $error->setErrorMessage(404, 'Unkown page');
-    $error->handle();
+if (empty ( $route )) {
+    $error = new ErrorAction ( $_lang );
+    $error->setErrorMessage ( 404, 'Unkown page' );
+    $error->handle ();
 }
 
 // does the route indicate an action?
-if (isset($route->params['action'])) {
+if (isset ( $route->params ['action'] )) {
     // take the action class directly from the route
-    $action = $route->params['action'];
+    $action = $route->params ['action'];
 } else {
     // use a default action class
     $action = 'index';
 }
 
+$action_class = ucfirst ( $action ) . 'Action';
 
-$action_class = ucfirst($action).'Action';
-
-if (!class_exists($action_class)) {
-    $error = new ErrorAction($_lang);
-    $error->setErrorMessage(400, 'Unkown action &lt;'.$action_class.'&gt;');
-    $error->handle();
+if (! class_exists ( $action_class )) {
+    $error = new ErrorAction ( $_lang );
+    $error->setErrorMessage ( 400, 'Unkown action &lt;' . $action_class . '&gt;' );
+    $error->handle ();
 }
 
-$actionClass = new $action_class($_lang);
+$actionClass = new $action_class ( $_lang );
 try {
     
-    $args = array_merge($route->params, $_REQUEST);
+    $args = array_merge ( $route->params, $_REQUEST );
     
-    if($actionClass->prepare($args)) {
-    	$actionClass->handle();
+    if ($actionClass->prepare ( $args )) {
+        $actionClass->handle ();
     }
-} catch(ClientException $e) {
-    $error = new ErrorAction($_lang);
-    $error->setErrorMessage($e->getCode(), 'ClientException:' . $e->getMessage());
-    $error->handle();
-} catch(ServerException $e) {
-    $error = new ErrorAction($_lang);
-    $error->setErrorMessage($e->getCode(), 'ServerException: '.$e->getMessage());
-    $error->handle();
-} catch(FluidframeException $e) {
-    echo $e->getTraceAsString();
-        $error = new ErrorAction($_lang);
-        $error->setErrorMessage(500, 'FluidframeException: '.$e->getMessage());
-        $error->handle();
-} catch(Exception $e) {
-    $error = new ErrorAction($_lang);
-    $error->setErrorMessage(500, 'Exception: '. $e->getMessage());
-    $error->handle();
+} catch ( ClientException $e ) {
+    $error = new ErrorAction ( $_lang );
+    $error->setErrorMessage ( $e->getCode (), 'ClientException:' . $e->getMessage () );
+    $error->handle ();
+} catch ( ServerException $e ) {
+    $error = new ErrorAction ( $_lang );
+    $error->setErrorMessage ( $e->getCode (), 'ServerException: ' . $e->getMessage () );
+    $error->handle ();
+} catch ( FluidframeException $e ) {
+    $error = new ErrorAction ( $_lang );
+    $error->setErrorMessage ( 500, 'FluidframeException: ' . $e->getMessage () );
+    $error->handle ();
+} catch ( Exception $e ) {
+    $error = new ErrorAction ( $_lang );
+    $error->setErrorMessage ( 500, 'Exception: ' . $e->getMessage () );
+    $error->handle ();
 }
 
 
