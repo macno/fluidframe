b65d90e485d9b6101eb44ac2fa4666f8c402185a
Federico Granata <federico@fluidware.it>
2015-10-05 14:28:27 +0200

    Implementazione iniziale della ricerca

--- action/adminroleapi.php
+++ action/adminroleapi.php
@@ -4,8 +4,16 @@ if (! defined ( 'FLUIDFRAME' )) {
 }
 class AdminroleapiAction extends Sbadmin2Action {
 
+    var $tableParams = array();
+
     function prepare($args) {
         parent::prepare($args);
+        $this->tableParams['draw']=$args['draw'];
+        $this->tableParams['columns']=$args['columns'];
+        $this->tableParams['order']=$args['order'];
+        $this->tableParams['start']=$args['start'];
+        $this->tableParams['length']=$args['length'];
+        $this->tableParams['search']=$args['search']['value'];
         return true;
     }
 
@@ -15,7 +23,37 @@ class AdminroleapiAction extends Sbadmin2Action {
         $this->API();
     }
 
+    function searchToSQL(&$obj,$search){
+        $schemaDef = $obj->schemaDef();
+        // common_debug("Obj: ".print_r($obj->schemaDef(),true));
+        // common_debug("Obj: ".print_r($obj->getAdminTableStruct(),true));
+        // common_debug("Search: ".$this->tableParams['search']['value']);
+        common_debug("Search: ".$search);
+        // common_debug("Searchable: ".(($obj->getAdminTableStruct()[2]['searchable']) ? 'true' : 'false'));
+        $searchParams = explode(":",$search);
+        $sql = '';
+        switch (count($searchParams)){
+            case 1: $cnt = 0; foreach($obj->getAdminTableStruct() as $col){
+                        if(isset($col['searchable']) && ($col['searchable'])){
+                            if(($schemaDef['fields'][$col['name']]['type'] == 'varchar') ||
+                                ($schemaDef['fields'][$col['name']]['type'] == 'text')){
+                                $sql .= ($cnt++ == 0) ? ' ' : ' OR ';
+                                $sql .= 'lower('.$col['name'] .') like \'%'. strtolower($searchParams[0]) .'%\'';
+                            }
+                        }
+                    }
+                    break;
+            // case 2: 
+            //         break;
+            // case 3: 
+            //         break;
+        }
+        common_debug("SQL: ".$sql);
+        return $sql;
+    }
+
     function API(){
+        // common_debug("Params: ".print_r($this->tableParams['columns'],true));
         $role = new Role();
         
         $tableCols = Role::getAdminTableStruct();
@@ -28,7 +66,31 @@ class AdminroleapiAction extends Sbadmin2Action {
             }
         }
         
+        $qry = "select count(*) as conta from ".$role->__table;
+        $role->query($qry);
+        $role->fetch();
+        $recordsTotal=$role->conta;
+
         $qry = "select " . implode(",", $sqlCols) . " from ".$role->__table;
+        $qryWhere = '';
+        if(!empty($this->tableParams['search'])){
+            $qryWhere = $this->searchToSQL($role,$this->tableParams['search']);
+            // $qry .= $this->searchToSQL($role,'admin');
+            if(strlen($qryWhere) > 0){
+                $qry .= " where ";
+                $qry .= $qryWhere ;
+            }
+        }
+
+        $qry .= " order by";
+        $cnt=0;
+        foreach($this->tableParams['order'] as $order){
+            $qry .= ($cnt++ == 0) ? ' ' : ', ';
+            $qry .= $this->tableParams['columns'][(int)$order['column']]['data'] . " " . $order['dir'];
+        }
+        $qry .= " offset ". $this->tableParams['start']*$this->tableParams['length'] ." limit ".$this->tableParams['length'];
+
+        common_debug("SQL: ".$qry);
         try {
             $role->query($qry);
         } catch (Exception $ex) {
@@ -42,6 +104,11 @@ class AdminroleapiAction extends Sbadmin2Action {
             }
             $roles[]=$row;
         }
-        echo json_encode(array('data'=>$roles));
+        echo json_encode(array(
+            'draw'=>(int)$this->tableParams['draw'],
+            'recordsTotal' => $recordsTotal,
+            'recordsFiltered' => 2,
+            'data'=>$roles
+        ));
     }
 }
