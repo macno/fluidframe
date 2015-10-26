e5907b2cedb2687371e1bf669376879066fda77f
Federico Granata <federico@fluidware.it>
2015-10-26 16:57:13 +0100

    Modificata la gestione delle traduzioni

--- lib/translator.php
+++ lib/translator.php
@@ -3,58 +3,25 @@ if (!defined('FLUIDFRAME')) {
 	exit(1);
 }
 
-$__t = array();
+$i18n = false;
 
-function _t($str,$context='GENERIC') {
-	global $_default_language, $_lang, $__t;
-	$strid = md5($str);
-	if(empty($context)) {
-	    $context = 'GENERIC';
-	}
-	if(isset($__t[$context][$strid])) {
-		return $__t[$context][$strid];
-	}
+function _i18n($context, $key, $deflt,$html = false) {
+    global $i18n, $_lang;
+    
+    if($i18n === false) {
 
-	$gettext = new Gettext();
-	$gettext->context=$context;
-	$gettext->lang=$_lang;
-	$gettext->str = $strid;
-	if(!$gettext->find(true)) {
-		// Add new line.
-		// Notify?
-		$gettext->created=common_sql_now();
-		$gettext->translation=  $str ;
-		$gettext->translated= ($_default_language == $_lang) ? 1 : 0;
-		$gettext->original_text = $str;
-		$gettext_id = $gettext->insert();
-		if(!$gettext_id) {
-		    throw new FluidframeException('Unable to insert translation row');
-		}
-	}
-	$__t[$context][$strid]=$gettext->translation;
-	return $gettext->translation;
+        $i = file_get_contents(INSTALLDIR.'/i18n/'.$_lang.'.json');
+        $i18n = json_decode($i,true);
+        common_debug(print_r($i18n,true));
+        if($_lang != common_config('site','language')) {
+            // Load default language as fallback..
+            $id = file_get_contents(INSTALLDIR.'/i18n/'.common_config('site','language').'.json');
+            $i18nd = json_decode($id,true);
+            $i18n = array_replace_recursive ($i18nd,$i18n);
+        }
+    }
+    if(isset($i18n[$context]) && isset($i18n[$context][$key])) {
+        return $i18n[$context][$key]['out'];
+    }
+    return $deflt;
 }
-
-function _tl($str,$context='GENERIC', $lang) {
- global $_default_language, $__t;
- $strid = md5($str);
-
- if(isset($__t[$context][$strid])) {
-  return $__t[$context][$strid];
- }
-
- $gettext = new Gettext();
- $gettext->context=$context;
- $gettext->lang=$lang;
- $gettext->str = $strid;
- if(!$gettext->find(true)) {
-  // Add new line.
-  // Notify?
-  $gettext->created=common_sql_now();
-  $gettext->translation=($_default_language == $lang) ? $str : '*'.$str.'*';
-  $gettext->insert();
- }
- $__t[$context][$strid]=$gettext->translation;
- return $gettext->translation;
-}
-
